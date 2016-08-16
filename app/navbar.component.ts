f3b1bde61d2e23d8615020211ce2b64c6d51b06a
Vincent Ma <siyan.ma@outlook.com>
2016-08-16 04:44:51 -0400

    added router links for navbar updated templates

--- app/navbar.component.ts
+++ app/navbar.component.ts
@@ -2,17 +2,20 @@
  * Created by vincentma on 8/15/16.
  */
 
-import {Component} from "angular2/src/core/metadata";
+import {Component} from "angular2/core";
+import {Router, ROUTER_DIRECTIVES} from "angular2/router";
 
 
 @Component({
-    selector: 'navbar',
-    templateUrl: 'app/templates/navbar.component.html'
+    selector: "navbar",
+    templateUrl: "app/templates/navbar.component.html",
+    directives: [ROUTER_DIRECTIVES],
 })
 export class NavBarComponent {
-    constructor() {
+    constructor(private _router: Router) { }
 
+    isCurrentRoute(route) {
+        var inst = this._router.generate(route);
+        return this._router.isRouteActive(inst);
     }
-
-
 }
